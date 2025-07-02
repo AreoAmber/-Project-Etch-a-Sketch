3ebaf1d41f6c7f944883a819b552decdbed2ad39
AreoAmber <areorolley@gmail.com>
2025-07-02 20:51:41 +0200

    Added the hover effect to display a pen like feature in webpage. Now we have the draw animantion

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 function createDivs() {
   const divContainer = document.querySelector(".container");
-
   for (let i = 1; i <= 256; i++) {
     let div = document.createElement("div");
     div.classList.add("square");
+    divContainer.appendChild(div);
+
+    div.addEventListener("mouseenter", () => {
+      div.style.backgroundColor = "red";
+    });
+
     divContainer.appendChild(div);
   }
 }
